e6aa6f1e4df910bfdc4ebb7bf3ca43ec977a8c45
wenshu.zhang <wenshu.zhang@ly.com>
2019-10-11 18:04:46 +0800

    添加gitlab源下载方式

--- bin/mktapp-init.js
+++ bin/mktapp-init.js
@@ -14,21 +14,27 @@ const rm = require('rimraf').sync;
 const Metalsmith = require('metalsmith')
 const tplPath = path.resolve(__dirname, '../template.json');
 const tplJson = require(tplPath);
+const co = require('co');
+const prompt = require('co-prompt');
 
 var cli = {
+
+	params:{
+		templateName:"",
+		projectName:"",
+	},
 
 	init:function(){
 		this.initProgram();
 		if(program.args.length < 2){
 			return program.help();
 		}
-		const template = program.args[0];
-		const dir = program.args[1];
-		if(!this.checkArgs(template,dir)){
+		this.params.templateName = program.args[0];	//模板名称
+		this.params.projectName = program.args[1];	//项目名称
+		if(!this.checkArgs()){
 			return;
 		}
-		this.initPrompt(dir,template);
-		//this.downLoadTemplate(template,dir);
+		this.initPrompt();
 	},
 
 	initProgram:function(){
@@ -47,12 +53,12 @@ var cli = {
 	/**
 	 * 检查参数
 	 */
-	checkArgs:function(template,dir){
-		if (!tplJson[template]) {
-			console.log(chalk.red(`template.json里没有${template}的模板信息，请添加！`));
+	checkArgs:function(){
+		if (!tplJson[this.params.templateName]) {
+			console.log(chalk.red(`template.json里没有${this.params.templateName}的模板信息，请添加！`));
 			return false;
 		}
-		if (!dir || dir.indexOf('/') > -1) {
+		if (!this.params.projectName || this.params.projectName.indexOf('/') > -1) {
 			console.log(chalk.red('请输入项目名名称'));
 			return false;
 		}
@@ -60,75 +66,92 @@ var cli = {
 	},
 
 	/**
-	 * 下载模板
-	 */
-	downLoadTemplate:function(template,dir){
-		console.log(chalk.yellow(`使用模板${template}创建项目`));
-		const spinner = ora('正在下载模板');
-		spinner.start();
-		exec(`npm i ${tplJson[template].npm}`, (err, data) => {
-			spinner.stop();
-			process.on('exit', () => {
-				rm(`${process.cwd()}/node_modules`)
-			})
-			if (err) {
-				console.log(chalk.red('模板下载失败 ', err.message));
-			}
-			//const tplPath = `${process.cwd()}/node_modules/${tplJson[template].npm}`;
-			const projectPath = `${process.cwd()}/${dir}`;
-			Metalsmith(process.cwd())
-				.source('.')
-				.destination(`${projectPath}`)
-				.build(function(err) {
-					if (err) {
-						console.log(chalk.red('项目生成失败', err));
-					} else {
-						console.log(chalk.yellow(' \n 项目已创建'));
-					}
-				}
-			);
-		});
-	},
-
-	/**
 	 * 初始化提示消息
 	 */
-	initPrompt:function(name,template){
+	initPrompt:function(){
 		inquirer.prompt([
 			{ name: 'description', message: '请输入项目描述' },
 			{ name: 'author', message: '请输入作者名称' }
 		]).then((answers) => {
-			console.log(chalk.yellow(`使用模板${template}创建项目`));
+			console.log(chalk.yellow(`使用模板${this.params.templateName}创建项目`));
 			const spinner = ora('正在下载模板...');
 			spinner.start(); //开始下载
-			this.downloadFramework(name,answers,spinner,template);
+			switch(tplJson[this.params.templateName].type){
+				case "github":
+					this.downloadGithub(answers,spinner);
+					break;
+				case "gitlab":
+					this.downloadGitlab(answers,spinner);
+					break;
+			}
 		});
 	},
 	
 	/**
-	 * 下载框架
+	 * github 下载
 	 */
- 	downloadFramework:function(name,answers,spinner,template){
-		download(tplJson[template].repo, name, (err) => {
+	downloadGithub:function(answers,spinner){
+		download(tplJson[this.params.templateName].repo, this.params.projectName, (err) => {
 			if(err){
 				spinner.fail(); //下载失败调用
+				console.log(symbols.error,chalk.red('\n 项目初始化失败 \n'));
 				console.log(symbols.error, chalk.red(err));
 			}else{
 				spinner.succeed(); //下载成功调用
-				const fileName = `${name}/package.json`;
-				const meta = {
-					name,
-					description: answers.description,
-					author: answers.author
-				};
-				if(fs.existsSync(fileName)){
-					const content = fs.readFileSync(fileName).toString();
-					const result = handlebars.compile(content)(meta);
-					fs.writeFileSync(fileName, result);
-				}
+				this.setPackageJsonFile(answers);
 				console.log(symbols.success, chalk.green('项目初始化完成'));
 			}
 		});
+	},
+
+	/**
+	 * gitlab 下载
+	 */
+	downloadGitlab(answers,spinner){
+		let gitlabUrl =  tplJson[this.params.templateName].repo;
+		let branch = tplJson[this.params.templateName].branch;
+		if (!gitlabUrl && !branch) {
+			halk.red('\n 没有配置项目地址 或 分支信息 \n')
+			return;
+		}
+		let cmdStr = `git clone ${gitlabUrl} ${this.params.projectName} && cd ${this.params.projectName} && git checkout ${branch}`
+
+		exec(cmdStr, (error, stdout, stderr) => {
+			if (error) {
+				spinner.fail(); //下载失败调用
+				console.log(symbols.error,chalk.red('\n 项目初始化失败 \n'));
+				console.log(symbols.error,chalk.red(error));
+				process.exit();
+			} else {
+				spinner.succeed(); //下载成功调用
+				this.setPackageJsonFile(answers);
+				console.log(symbols.success, chalk.green('项目初始化完成'));
+				process.exit();
+			}
+		})
+	},
+
+	/**
+	 * 设置package.json文件
+	 */
+	setPackageJsonFile(answers){
+		let filePath = "";
+		switch(this.params.templateName){	//特殊处理
+			case "webHorse":
+				filePath = "/web-horse";
+				break;
+		}
+		const file = `${this.params.projectName}${filePath}/package.json`;
+		const meta = {
+			name:this.params.projectName || "app_horse_vue",
+			description: answers.description || "初始化框架",
+			author: answers.author ||'wenshu.zhang(1201500)'
+		};
+		if(fs.existsSync(file)){
+			const content = fs.readFileSync(file).toString();
+			const result = handlebars.compile(content)(meta);
+			fs.writeFileSync(file, result);
+		}
 	}
 
 };
